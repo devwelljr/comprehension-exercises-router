bd0300f654e3e185e2f5ec5b32f35599cfe7ed7d
Wellington <junior.wel02@exemplo.br>
2021-07-05 18:36:10 -0300

    7 a 9

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
+import { Switch, Route, Link, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
+    const greetingsMessage = 'Good Morning';
     return (
       <BrowserRouter>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/users'>About</Link>
-        </div>
-        <Home />
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/users'>Users</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route
+            path='/users/:id'
+            render={(props) => (
+              <Users { ...props } greetingsMessage={ greetingsMessage } />
+            )}
+          />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props;
+    const { id } = this.props.match.params;
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
-        <Link to="/">Home</Link>
+        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> { id }, este é seu ID!</p>
       </div>
     );
   }
-};
+}
 
 export default Users;
