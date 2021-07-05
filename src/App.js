8bf334a54c60eacb91a1c8e464f9455b3b479d43
Wellington <junior.wel02@exemplo.br>
2021-07-05 15:54:09 -0300

    1 a 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/users'>About</Link>
+        </div>
+        <Home />
+      </BrowserRouter>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router';
+import About from './About.js';
+import Users from './Users.js';
 
 class Home extends Component {
   render() {
@@ -6,6 +9,9 @@ class Home extends Component {
       <div>
         <h2>Home</h2>
         <p> My awesome Home component </p>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/users" component={Users}/>
       </div>
     );
   }
